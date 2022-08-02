0bf36d0207f5e1ce84f4ea3c2b935882028d4429
varun <varun@varuns-MacBook-Pro.local>
2022-08-02 21:19:18 +0530

    code urls

--- deployer.groovy
+++ deployer.groovy
@@ -3,12 +3,12 @@ pipeline
 	agent { label 'master' }
 	environment
 	{
-		DEV_CLONE_URL = "git@github.com:hyrglobalsource/optjobs.git"
+		DEV_CLONE_URL = "https://github.com/hyrglobalsource/optjobs.git"
 		DEV_DIR = "$WORKSPACE" + "/devCode"
 		DEVOPS_DIR = "$WORKSPACE" + "/devopsCode"
 		SKIP_TLS = true
 		DEPLOY_ENV = "staging"
-		DEV_BRANCH = ""
+		DEV_BRANCH = "main"
 
 
 	}
@@ -25,12 +25,20 @@ pipeline
 			{
 				script
 				{
-					stage('CHECKOUT DEV CODE')
+					stage('CHECKOUT DEVOPS CODE')
 					{
-						dir(DEV_DIR)
+						dir(DEVOPS_DIR)
 						{
 							checkout scm
 							//checkout([$class: 'GitSCM', branches: [[name: DEV_BRANCH]], extensions: [], userRemoteConfigs: [[url: DEV_CLONE_URL]]])
+						}
+
+					}
+					stage('CHECKOUT DEV CODE')
+					{
+						dir(DEVOPS_DIR)
+						{
+							checkout([$class: 'GitSCM', branches: [[name: DEV_BRANCH]], extensions: [], userRemoteConfigs: [[url: DEV_CLONE_URL]]])
 						}
 
 					}
